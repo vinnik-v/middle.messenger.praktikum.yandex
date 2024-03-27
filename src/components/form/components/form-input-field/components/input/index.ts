fcdf9dbf93f4e68cfda233e0ab257b7f45addd32
= <vinnikviv@gmail.com>
2024-03-27 18:23:35 +0300

    fix

--- src/components/form/components/form-input-field/components/input/index.ts
+++ src/components/form/components/form-input-field/components/input/index.ts
@@ -1,14 +1,13 @@
 import './input.scss';
 import Block from "../../../../../../classes/Block";
-
 export default class InputElem extends Block {
-    constructor(props: Record<string, string | string[] | Record<string, | Block | boolean | ((event: Event) => unknown)> | { name: string, value: string}[]>) {
+    constructor(props: Record<string, string | string[] | Record<string, | Block | boolean | ((event: Event) => unknown)> | { name: string, value: string }[]>) {
         const tagName = {
             tagName: 'input'
         }
         const className = {
             className: 'form-input-field__input'
         }
-        super('', {...tagName, ...className, ...props});
+        super('', { ...tagName, ...className, ...props });
     }
 }
