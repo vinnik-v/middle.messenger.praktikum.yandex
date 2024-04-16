94fc2fb572a30190f7b16f4376cf8f2fef25a48c
= <vinnikviv@gmail.com>
2024-04-16 21:19:44 +0300

    правки линтера

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import './index.scss';
 import dropdownClickListener from './functions/dropdownClickListener';
-// import changeProfileDataHandle from './functions/changeProfileDataHandle';
-// import changePasswordHandle from './functions/changePasswordHandle';
 import { routerInit } from './classes/Router';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,8 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('click', (e: Event) => {
 
-  // changePasswordHandle(e);
-  // changeProfileDataHandle(e);
   dropdownClickListener(e, '#attach-file-button', 'choose-file-dropdown');
   dropdownClickListener(e, '#chat-window-header-button', 'chat-window-header-dropdown');
   dropdownClickListener(e, '#add-user-button', 'add-user-modal');
